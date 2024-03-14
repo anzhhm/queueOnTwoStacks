fb8faf021d713dd17c235407d3105546da39f66a
Anzhelika-Mariia <angelika11hr@gmail.com>
2024-03-14 10:54:43 +0200

    Solution added.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _120324
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StacksToQueue<int> stackIsTheNewQueue = new StacksToQueue<int>();
+           // stackIsTheNewQueue.Dequeue();
+
+            stackIsTheNewQueue.AddToQueue(1);
+            stackIsTheNewQueue.AddToQueue(2);
+            stackIsTheNewQueue.AddToQueue(3);
+
+            stackIsTheNewQueue.Dequeue();
+            stackIsTheNewQueue.Dequeue();
+
+            stackIsTheNewQueue.AddToQueue(4); ;
+
+        }
+        
+        
+    }
+}
---
+++ StacksToQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _120324
+{
+    public class StacksToQueue<T>
+    {
+        private Stack <T> inbox = new Stack <T> ();
+        private Stack <T> outbox = new Stack <T> ();
+        public StacksToQueue(){  }
+
+        public void AddToQueue(T elem)
+        {
+            inbox.Push(elem);
+        }
+        public void Dequeue()
+        {
+            if (inbox.Count == 0&& outbox.Count==0) {
+                throw new Exception("The queue is empty");
+            }
+            if (outbox.Count == 0)
+            {
+                while (inbox.Count > 0) {
+                    T element = inbox.Pop();
+                    outbox.Push(element);
+                }
+                outbox.Pop();
+            }
+            else
+            {
+                outbox.Pop ();
+            }
+        }
+    }
+}
